db6f520ddaf83280632d06606149862128c42eed
Arkadiusz Sliwowski <arek.sliwowski37@gmail.com>
2023-05-29 14:44:02 +0200

    New logic

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -2,10 +2,10 @@ import '../App.css';
 import {useState} from 'react';
 export default function Board(){
     //create grid
-    const rows = 10;
-    const cols = 10;
+    const rows = 30;
+    const cols = 30;
 
-    const [grid, setGrid] = useState(() => {
+    let [grid, setGrid] = useState(() => {
         const gridRows = [];
         for(let i = 0; i< rows; i++){
             gridRows.push(Array.from(Array(cols), () => 0))
@@ -13,42 +13,47 @@ export default function Board(){
         return gridRows
     })
 
-    // console.table(grid)
     // Start game
+    const neighborsOperations = [
+        [1, 0],
+        [-1, 0],
+        [0, 1],
+        [0, -1],
+        [1, -1],
+        [-1, 1],
+        [1, 1],
+        [-1, -1]
+    ]
     function startGame(){
-        let newGrid = [...grid]
         setInterval(() => {
-            for(let i = 0; i < rows; i++){
-                for(let j = 0; j < cols; j++){
-                    let neighbors = 0; // neighbors of current cell
-    
-                    if(i === 0 || i === rows - 1 || j === 0 || j === cols - 1){
-                        newGrid[i][j] = grid[i][j];
-                    } else {
-                        neighbors += newGrid[i+1][j-1];
-                        neighbors += newGrid[i+1][j];
-                        neighbors += newGrid[i+1][j+1];
-                        neighbors += newGrid[i][j-1];
-                        neighbors += newGrid[i][j+1];
-                        neighbors += newGrid[i-1][j-1];
-                        neighbors += newGrid[i-1][j];
-                        neighbors += newGrid[i-1][j+1];
-                    }
-                    
-                    if(neighbors < 2 || neighbors > 3){
-                        newGrid[i][j] = 0
-                    } else if(grid[i][j] === 0 && neighbors === 3){
-                        newGrid[i][j] = 1;
-                    }
-                }
-            }
-            setGrid(newGrid)
-        }, 1000)        
-        // console.log(grid)
+            setGrid((grid) => {
+                const newGrid = grid.map((row, i) => {
+                    return row.map((col, j) => {
+                        let neighbors = 0;
+                        neighborsOperations.forEach(([x, y]) => {
+                            const neighborI = i + x;
+                            const neighborJ = j + y;
+                            if(neighborI >= 0 && neighborI < rows && neighborJ >= 0 && neighborJ < cols){
+                                neighbors += grid[neighborI][neighborJ]
+                            }
+                        })
+                        if(neighbors > 3 || neighbors < 2){
+                            return 0;
+                        } else if(neighbors === 3){
+                            return 1;
+                        } else {
+                            return grid[i][j]
+                        }
+                    })
+                })
+                return newGrid;
+            })
+        }, 1000)
     }
 
     return(
     <div>
+    <button onClick={startGame}>START</button>
     <div style={{
         display: 'grid',
         gridTemplateColumns: `repeat(${cols}, 35px)`
@@ -65,7 +70,6 @@ export default function Board(){
                     ></div>)) //generate each cell
         }
     </div>
-    <button onClick={startGame}>START</button>
     </div>
     )
 }
